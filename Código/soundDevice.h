498f9235b72de159d7119eb0fe06d6776c5d9ade
NIU1710748 <149066007+NIU1710748@users.noreply.github.com>
2025-10-16 14:12:04 +0200

    Archivos organized

---
+++ Código/soundDevice.h
@@ -0,0 +1,16 @@
+
+#include <AL\alc.h>
+
+class SoundDevice
+{
+public:
+	static SoundDevice* get();
+
+private:
+	SoundDevice();
+	~SoundDevice();
+
+	ALCdevice* p_ALCDevice;
+	ALCcontext* p_ALCContext;
+
+};
